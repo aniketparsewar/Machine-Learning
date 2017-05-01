4599854fad6c3e0655684935aa80abb0330c7961
aniketparsewar <aniketparsewar@yahoo.co.in>
2017-05-01 19:06:23 -0400

    Add files via upload

---
+++ Churn_Modelling.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 19:36:38 2017
+
+@author: Aniket
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:,1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:,2])
+onehotencoder = OneHotEncoder(categorical_features= [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[: , 1:]
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+# ANN
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+
+
+#Initialize
+classifier = Sequential()
+
+
+#Adding Layers with dropout
+classifier.add(Dense(units = 6, activation = 'relu', kernel_initializer = 'uniform',  input_dim = 11))
+classifier.add(Dropout(p = 0.1))
+
+#second Layer
+classifier.add(Dense(units = 6, activation = 'relu', kernel_initializer = 'uniform'))
+classifier.add(Dropout(p = 0.1))
+
+#output Layer
+classifier.add(Dense(units = 1, activation = 'sigmoid', kernel_initializer = 'uniform'))
+
+
+#Complie ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy' , metrics = ['accuracy'])
+
+
+#Fit to model
+classifier.fit(X_train,y_train, batch_size = 10, nb_epoch = 100)
+
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+
+#Evaluate ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(units = 6, activation = 'relu', kernel_initializer = 'uniform',  input_dim = 11))
+    classifier.add(Dense(units = 6, activation = 'relu', kernel_initializer = 'uniform'))
+    classifier.add(Dense(units = 1, activation = 'sigmoid', kernel_initializer = 'uniform'))
+    classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy' , metrics = ['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn = build_classifier, batch_size = 10, nb_epoch = 100)
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10, n_jobs = -1)
+mean = accuracies.mean()
+variance = accuracies.std()
+
+#Improving ANN
+#Dropout regularization
+
+#Tunning ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+def build_classifier(optimizer, unit):
+    classifier = Sequential()
+    classifier.add(Dense(units = unit, activation = 'relu', kernel_initializer = 'uniform',  input_dim = 11))
+    classifier.add(Dense(units = unit, activation = 'relu', kernel_initializer = 'uniform'))
+    classifier.add(Dense(units = 1, activation = 'sigmoid', kernel_initializer = 'uniform'))
+    classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy' , metrics = ['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn = build_classifier)
+parameters = {'batch_size': [25, 32, 15, 20],
+              'nb_epoch': [100, 500, 400],
+              'optimizer': ['adam', 'rmsprop'],
+              'unit': [6, 8, 10]}
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10)
+grid_search = grid_search.fit(X_train, y_train)
+best_parameters = grid_search.best_params_ 
+best_accuracy = grid_search.best_score_
+
+
+
+
+
+
+
+
+
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10, n_jobs = -1)
+mean = accuracies.mean()
+variance = accuracies.std()
+
+
+
+
